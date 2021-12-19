5735836cebe4086609631ad718fafbddd551bd09
troy94728 <zlzlskfl13@naver.com>
2021-12-19 11:19:23 +0900

    object

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log(1 + 1);
+const playerName = "gon";
+const playerPoints = 1212;
+const playerHandsome = false;
+const playerFat = "little bit";
+
+const player = {
+  name: "gongon",
+  points: 10,
+  fat: true,
+};
+
+console.log(player);
