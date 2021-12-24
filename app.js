ba76ca43248eb4358d3bad029e90939563b8a38d
troy94728 <zlzlskfl13@naver.com>
2021-12-24 17:07:06 +0900

    elements

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const players = {
     fat: true,
 };
 
-console.log(players);
+//console.log(players);
 players.points = 10;
-console.log(players);
+//console.log(players);
 //players를 const로 상수처리 햇는데 왜 players.points로 업데이트할 수 있지?
 
 function plus(a, b) {
@@ -36,7 +36,7 @@ const calculator = {
         return a ** b;
     },
 };
-plus(5, 10);
+//plus(5, 10);
 
 const age = 96;
 
@@ -46,7 +46,7 @@ function calculateKrAge(ageOfForeigner) {
 
 const krAge = calculateKrAge(age);
 
-console.log(krAge);
+//console.log(krAge);
 
 const plusResult = calculator.plus(2, 3);
 const minusResult = calculator.minus(plusResult, 3);
@@ -54,13 +54,43 @@ const timesResult = calculator.times(10, minusResult);
 const divideResult = calculator.divide(timesResult, plusResult);
 const powerResult = calculator.power(divideResult, minusResult);
 
-console.log("==");
-console.log(plusResult);
-console.log(minusResult);
-console.log(timesResult);
-console.log(divideResult);
-console.log(powerResult);
+// console.log("==");
+// console.log(plusResult);
+// console.log(minusResult);
+// console.log(timesResult);
+// console.log(divideResult);
+// console.log(powerResult);
 
-const ageAge = prompt("how old are you");
+const ageAge = parseInt(prompt("how old are you"));
 
-console.log(typeof ageAge, typeof parseInt(ageAge));
+//console.log(typeof ageAge, typeof parseInt(ageAge));
+
+//console.log(isNaN(ageAge));
+//==> true
+
+// if (isNaN(ageAge) || age < 0) {
+//     console.log("please write a real positive number");
+// } else if (age < 18) {
+//     console.log("you are too young");
+// } else if (age > 18 && age <= 50) {
+//     console.log("you can drink");
+// } else if (age > 50 && age <= 80) {
+//     console.log("you should exercise");
+// } else if (age > 80) {
+//     console.log("you can whatever you want");
+// } else if (age == 100) {
+//     console.log("you are wise");
+// } else {
+//     console.log("thank you");
+// }
+
+const title = document.getElementById("title");
+
+title.innerText = "Got you!"; //js로 html변경
+
+console.log(title.id);
+//id찾아감
+//id이름 출력
+console.log(title.className);
+//class찾아감
+//class이름 출력
