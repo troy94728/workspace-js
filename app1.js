db21874c36773a83ab4dce9684ae16bbbd026e9f
troy94728 <zlzlskfl13@naver.com>
2021-12-28 22:50:06 +0900

    idClassObject

--- app1.js
+++ app1.js
@@ -1,8 +1,15 @@
 const title = document.querySelector("div.hello:first-child h1");
 // 선택자 다양하게 가능
 
+const hellos = document.getElementsByClassName("hello");
 // console.dir(title);
+console.log(hellos);
 // 객체까지 다 출력함
+
+title.innerText = "got you";
+
+console.log(title.id);
+console.log(title.className);
 
 function handleTitleClick() {
     console.log("title was clicked");
