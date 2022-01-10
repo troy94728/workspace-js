3f5b525f1257b9e6b3d606a7fd0abfcf767f1dd0
troy94728 <zlzlskfl13@naver.com>
2022-01-11 01:22:28 +0900

    reset

--- app1.js
+++ app1.js
@@ -1,21 +1,6 @@
-const title = document.querySelector("div.hello:first-child h1");
-// 선택자 다양하게 가능
+const title = document.getElementById("title");
 
-const hellos = document.getElementsByClassName("hello");
-// console.dir(title);
-console.log(hellos);
-// 객체까지 다 출력함
-
-title.innerText = "got you";
+title.innerText = "get you!";
 
 console.log(title.id);
 console.log(title.className);
-
-function handleTitleClick() {
-    console.log("title was clicked");
-}
-
-title.style.color = "blue";
-// 프로퍼티 바꾸기
-
-title.addEventListener("click", handleTitleClick);
